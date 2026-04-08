69f96e93565c0a385bc6e208e4dc42bf0720d080
Abhinav Shankar <abhinav.shankar17@gmail.com>
2026-04-08 09:14:57 +0530

    UC10: Iterate bogies using forEach

--- src/main/TrainConsistApp.java
+++ src/main/TrainConsistApp.java
@@ -17,8 +17,6 @@ public class TrainConsistApp {
                 new Bogie("AC", 50)
         );
 
-        bogies.stream()
-                .filter(b -> b.capacity > 60)
-                .forEach(b -> System.out.println(b.type));
+        bogies.forEach(b -> System.out.println(b.type));
     }
 }
--- src/test/test.java
+++ src/test/test.java
@@ -5,13 +5,12 @@ import java.util.*;
 public class TrainConsistAppTest {
 
     @Test
-    void testFilterCapacity() {
+    void testIteration() {
         List<Bogie> list = Arrays.asList(
                 new Bogie("Sleeper", 72),
                 new Bogie("AC", 50)
         );
 
-        long count = list.stream().filter(b -> b.capacity > 60).count();
-        assertEquals(1, count);
+        assertEquals(2, list.size());
     }
 }
